8480bb1b268f12ad513b676767f22a67be385b99
Jason Dippel <jason.dippel@hootsuite.com>
2020-03-04 22:07:05 -0800

    Just enough to see if it would work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useTheme, setThemeVal } from './theme'
+
+window.setThemeVal = setThemeVal
+
+const H1 = styled.h1`
+  color: ${useTheme('light')};
+`
 
 export const App = () => (
   <div>
-    <h1>
+    <H1>
       Rockets are a go!{' '}
       <span role="img" aria-label="rocket emoji">
         🚀
       </span>
-    </h1>
+    </H1>
     <p>
       Congrats! You got a simple react app up and running{' '}
       <span role="img" aria-label="party popper emoji">
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,25 @@
-export const useTheme = () => {
+const generateKey = rawPathToKey => rawPathToKey
+
+/**
+ * Updates all values in the document to reflect the values in the provided theme; Full replacement
+ * @param {object} themeObj
+ */
+export const setActiveTheme = themeObj => {
   console.log('this does not do anything yet...')
 }
+
+/**
+ * Updates specific key for the active theme to the provided value; Partial replacement
+ * @param {string} key
+ * @param {string} val
+ */
+export const setThemeVal = (key, val) => {
+  document.documentElement.style.setProperty(`--${generateKey(key)}`, val)
+}
+
+/**
+ * Returns the value of a key that belongs to the theme
+ * @param {string} pathToVal
+ * @returns {string | undefined} themeValue
+ */
+export const useTheme = pathToVal => `var(--${generateKey(pathToVal)})`
