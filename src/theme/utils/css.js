a54e92401672c567469a8768b49772582cdbe391
Jason Dippel <jason.dippel@hootsuite.com>
2020-03-08 12:35:15 -0700

    Setup interface for creating/updating style block

--- src/theme/utils/css.js
+++ src/theme/utils/css.js
@@ -1,6 +1,24 @@
 import { isIE } from './index'
 
 /**
+ * Gets a reference to the property style block if it exists
+ * @returns {Element | undefined} HTML element or undefined
+ */
+const maybeGetPropertyStyleBlock = () => {}
+
+/**
+ * Creates a style block in the header for all theme values to be specified in
+ */
+const createPropertyStyleBlock = () => {
+  const head = document.head || document.getElementsByTagName('head')[0]
+  const StyleBlock = document.createElement('style')
+  StyleBlock.setAttribute('data-themeProperties', true)
+  head.appendChild(StyleBlock)
+}
+
+const removePropertyStyleBlock = () => {}
+
+/**
  * Creates a named css property for use in the document
  * @param {string} name
  * @param {string} value
@@ -12,3 +30,5 @@ export const createProperty = (name, value) => {
   if (isIE()) return
   document.documentElement.style.setProperty(`--${name}`, value)
 }
+
+const removeProperty = () => {}
